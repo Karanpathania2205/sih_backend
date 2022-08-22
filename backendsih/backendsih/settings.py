f40b56fbf185df2a737ac6ddc69699bb53553c18
Karan Singh Pathania <singhpathaniakaran@gmail.com>
2022-08-22 15:30:42 +0530

    second

--- backendsih/backendsih/settings.py
+++ backendsih/backendsih/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "imageclassifier"
 ]
 
 MIDDLEWARE = [
@@ -114,6 +115,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
 ]
+MEDIA_URL='/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- backendsih/imageclassifier/urls.py
+++ backendsih/imageclassifier/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from imageclassifier import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path("",views.register,name="register"),
@@ -24,3 +26,4 @@ urlpatterns = [
     path("logout",views.logout,name="logout")
     
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
